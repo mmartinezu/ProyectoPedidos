32423025ba182cef4c41326722949c5afdce9fcc
jmartinez6208 <jmartinez6208@uta.edu.ec>
2022-01-09 14:16:24 -0500

    Diseno Pedidos

--- PresentacionMovil/MainActivity.cs
+++ PresentacionMovil/MainActivity.cs
@@ -1,7 +1,9 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using AndroidX.AppCompat.App;
+using System;
 
 namespace PresentacionMovil
 {
@@ -14,12 +16,36 @@ namespace PresentacionMovil
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            botones();
+            
         }
+
+        private void botones()
+        {
+            var registrarseBoton = FindViewById(Resource.Id.btnRegistrarse);
+            registrarseBoton.Click += Registrarse_Click;
+            var iniciarSesionBoton = FindViewById(Resource.Id.btnIngresar);
+            iniciarSesionBoton.Click += Iniciar_Click;
+        }
+
+        private void Registrarse_Click(object sender, EventArgs e)
+        {
+            var intent = new Intent(this, typeof(RegistroActivity));
+            StartActivity(intent);
+        }
+        private void Iniciar_Click(object sender, EventArgs e)
+        {
+            var intent = new Intent(this, typeof(PedidosActivity));
+            StartActivity(intent);
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
+
+        
     }
 }
---
+++ PresentacionMovil/RegistroActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AndroidX.AppCompat.App;
+
+namespace PresentacionMovil
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class RegistroActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_registro);
+            // Create your application here
+        }
+    }
+}
