ecaa84e2ff9845438604829f5835f8761b50bfff
jmartinez6208 <jmartinez6208@uta.edu.ec>
2022-01-11 17:32:42 -0500

    Interfaces cliente listas

---
+++ PresentacionMovil/ProductosActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PresentacionMovil
+{
+    [Activity(Label = "ProductosActivity")]
+    public class ProductosActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_productos);
+            // Create your application here
+        }
+    }
+}
